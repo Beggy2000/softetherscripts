9c6a13337dce3ac492332833f7b69d6c3475caf5
Beggy2000 <BeggyCode@gmail.com>
2019-10-09 19:40:07 +0200

    reduce package list

--- script/installServer.sh
+++ script/installServer.sh
@@ -19,7 +19,7 @@
 
 source "$(dirname "$0")/function.sh" || { echo "Critical error: there is no library $(dirname "$0")/function.sh" >&2 ; exit 1; }
 
-declare -a packageList=("build-essential" "curl" "gcc" "git" "libncurses-dev" "libreadline-dev" "libssl-dev" "make" "tzdata" "wget" "zlib1g-dev")
+declare -a packageList=("build-essential")
 checkInstallation "${packageList[@]}" || stopTheScript "" 1
 
 checkIfRoot || stopTheScript "You must start the script as root (sudo)." 1
